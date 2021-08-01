2985107503be7390504c7c55d793d97287357381
Rafael Gamez Diaz <rafaelgamezdiaz@gmail.com>
2021-08-01 01:07:19 -0400

    User validations pipes implemented. Continuous with user CRUD (not finished)

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,36 @@
-import { UsersStatus } from '../enums/users-status.enum';
+import { IsAlpha, IsEmail, IsNotEmpty } from "class-validator";
 
 export class CreateUserDto {
+
+  @IsNotEmpty()
+  @IsAlpha()
   name: string;
+
+  @IsAlpha()
   surname: string;
+
+  @IsAlpha()
   lastname: string;
+
+  @IsAlpha()
   gender: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
+
   phone: string;
+
   address: string;
+
   age: number;
+
   group: string; // speleological group
+
+  @IsNotEmpty()
+  @IsAlpha()
   role: string;
 }
